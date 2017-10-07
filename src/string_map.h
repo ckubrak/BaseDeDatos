558292a5f27dbe7063444b3cbd2fd69d909a8fc9
Pedro Boitier <pedrovboitier@gmail.com>
2017-10-07 20:19:32 -0300

    primera implementacion de string map, pepi y jordi

--- src/string_map.h
+++ src/string_map.h
@@ -12,34 +12,107 @@ using std::pair;
 /**
  * Implementacion de map<string,T> sobre Trie
  * Asume de T:
- * - tiene constructor por copia 
+ * - tiene constructor por copia
  * - tiene operador ==
  * - solo permite utilizar el operator[] si T tiene constructor por defecto
  */
 template < typename T >
 class string_map {
-public:
+
     typedef string key_type;
     typedef T mapped_type;
-    typedef std::pair<const key_type, mapped_type> value_type;
+    typedef std::pair<const key_type, mapped_type> value_type;  //para que usamos value_type?
     typedef size_t size_type;
 
+private:
 
-    class iterator;
-    class const_iterator;
-    
+    struct Nodo{
+        Nodo(key_type id, mapped_type* sig, Nodo* pad,Nodo* prox) :  id(id), significado(sig), padre(pad),proximo(prox){};
+        key_type id;
+        mapped_type* significado;
+        Nodo* padre;
+        Nodo* proximo;
+        Nodo* hijos[27];  // Pusimos 27 porque suponemos que las claves solo usan caracteres tradicionales (letras en espaniol).
+    };
+
+    size_type nroClaves;
+    Nodo* iniciosClaves[27] ;
+
+    //metodos privados
+    Nodo* minimaClave(Nodo* nodo){
+        /*Nodo* actual = nodo;
+        while(actual->significado == nullptr){
+            actual = actual.hijos[0];
+        }
+        return actual;*/
+
+        //arreglarlo porque ahora hijos es un array!
+    }
+
+
+public:
+
+
+    class iterator{
+    public:
+        typedef T value_type;
+        iterator(const iterator&){};
+        iterator& operator=(const iterator&);
+
+        bool operator==(const iterator&) const;
+        bool operator!=(const iterator&) const;
+
+        iterator& operator++();
+        iterator operator++(int);
+
+        value_type& operator*() const;
+        value_type /* * */ operator->() const;
+
+        friend class string_map;
+
+    private:
+        iterator(string_map::Nodo* n): nodo(n){}
+        Nodo* nodo;
+
+    };
+
+    class const_iterator{
+    public:
+        typedef T value_type;
+        const_iterator(const const_iterator&){};
+        const_iterator& operator=(const const_iterator&);
+
+        bool operator==(const const_iterator&) const;
+        bool operator!=(const const_iterator&) const;
+
+        const_iterator& operator++();
+        const_iterator operator++(int);
+
+        value_type& operator*() const;
+        value_type* operator->() const;
+
+        friend class string_map;
+
+    private:
+        const_iterator(string_map::Nodo* n): nodo(n){}
+        Nodo* nodo;
+    };
+
 
     /** @brief Construye mapa vacio
      *
      * \complexity{\O(1)}
      */
-    string_map();
+    string_map(): nroClaves(0){}; //hace falta pasar el vector vacio o
+    //se inicializa solo?
 
     /** @brief Destruye mapa
      *
      * \complexity{\O(sn * S)}
      */
-    ~string_map();
+    ~string_map(){
+        return;
+    }
 
     /** @brief Constructor por copia
      *
@@ -68,10 +141,14 @@ public:
     size_type count(const key_type &key) const;
 
     /** @brief Devuelve cantidad de claves definidas */
-    size_t size() const;
+    size_t size() const{
+        return nroClaves;
+    }
 
     /** @brief devuelve true si size() == 0 */
-    bool empty() const;
+    bool empty() const{
+        return nroClaves == 0;
+    }
 
 
     /** @brief Acceso / definición de pares clave/valor
@@ -95,7 +172,7 @@ public:
      *  @returns una referencia const a la definicion.
      *
      *  \complexity{\O(S)}
-     */ 
+     */
     const mapped_type& at(const key_type& key) const;
 
     /** @brief Vacia el mapa */
@@ -108,19 +185,32 @@ public:
      *
      *  \complexity{\O(S)}
      */
-   iterator begin();
+    iterator begin(){
+        if(nroClaves == 0){
+            return end();
+        }
+        else{
+            Nodo* actual = iniciosClaves[0];
+            while(actual->significado == nullptr){
+                actual = actual->hijos[0];
+            }
+            return iterator(actual);
+        }
+    }
 
     /*  @brief iterador al fin de la coleccion
      *
      *  \complexity{\O(S)}
      */
-   iterator end();
+    iterator end(){
+        return iterator(nullptr);
+    }
 
     /// Versiones const de begin/end
-   const_iterator begin() const;
-   const_iterator end() const;
-   const_iterator cbegin() const;
-   const_iterator cend() const;
+    const_iterator begin() const;
+    const_iterator end() const;
+    const_iterator cbegin() const;
+    const_iterator cend() const;
 
     /** @brief busca una clave
      *  @param key clave a buscar
@@ -128,7 +218,7 @@ public:
      *
      *  \complexity{\O(S)}
      */
-   iterator find(const key_type &key);
+    iterator find(const key_type &key);
 
     /** @brief busca una clave
      *  @param key clave a buscar
@@ -136,15 +226,17 @@ public:
      *
      *  \complexity{\O(S)}
      */
-   const_iterator find(const key_type &key) const;
+    const_iterator find(const key_type &key) const;
 
     /** @brief insercion
      * @param value par <clave,significado> a insertar, no debia existir la clave
      * @returns un par con el iterador resultante y
-     * 
+     *
      * \complexity{\O(S + copy(value_type))}
      */
-    pair<iterator,bool> insert(const value_type &value);
+    pair<iterator,bool> insert(const value_type &value){
+
+    }
 
     /** @brief eliminar una clave
      *  @param key clave a eliminar
@@ -162,9 +254,35 @@ public:
      */
     iterator erase(iterator pos);
 
-private:
 
 };
+
+template<class T>
+typename string_map<T>::iterator& string_map<T>::iterator::operator++(){
+    /*if (nodo.hijos.size() > 0) {
+        return iterator(minimaClave(nodo.hijos[0]));
+    }else if(nodo.proximo != nullptr){
+        return iterator(minimaClave(nodo.proximo));
+    }else{
+        Nodo* actual = nodo;
+        while(actual->proximo == nullptr){
+            if(nodo.padre != nullptr){
+                actual = actual->padre;
+            }else{
+                return end();
+            }
+        }
+        return iterator(minimaClave(actual->proximo));
+    }
+
+     arreglarlo porque ahora hijos es un array!
+    */
+}
+
+template<class T>
+typename string_map<T>::iterator::value_type string_map<T>::iterator::operator->() const{
+    return *nodo;
+}
 
 
 #endif //STRING_MAP_STRING_MAP_H
