482870a5889c18479d25ccd5e1b16a439495a898
Cristian Kubrak <kubrakcristian@gmail.com>
2017-10-19 22:59:07 -0300

    Falta la complejidad de agregarRegistro que no me da. Hay que checkear la de crearIndice

--- src/BaseDeDatos.cpp
+++ src/BaseDeDatos.cpp
@@ -15,7 +15,14 @@ void BaseDeDatos::crearTabla(const string &nombre,
 
 void BaseDeDatos::agregarRegistro(const Registro &r, const string &nombre) {
   Tabla &t = _tablas.at(nombre);
- if (_indices.count(nombre)){
+  if (_indices.count(nombre)){
+  //for (auto i : _indices.at(nombre)){
+  // if (i.esNat()){
+  //   for (auto )
+  //   string_map<linear_set <Registro> >::value_type nReg = make_pair(r.valorStr(),registro);
+  //   i._diccInt.insert()
+  // }
+  //}
    //Modificar indices
    // bool esNat = _indices[nombre].tipo();
    // set<Registro> registros;
@@ -179,7 +186,7 @@ void BaseDeDatos::crearIndice(const string &nombre, const string &campo){
       if (indiceNuevo._diccString.count(d.valorStr())){
         indiceNuevo._diccString.at(d.valorStr()).fast_insert(r);
       } else {
-        string_map<linear_set <Registro> >::value_type nReg = make_pair(d.valorStr(),registro);
+        i.
         indiceNuevo._diccString.insert(nReg);
       }
     }
