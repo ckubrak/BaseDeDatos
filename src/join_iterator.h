c64c55bda054cd8a0f5f51bca90c9d823a1e1c9f
Cristian Kubrak <kubrakcristian@gmail.com>
2017-10-22 20:49:52 -0300

    No anda. Quiero merca

--- src/join_iterator.h
+++ src/join_iterator.h
@@ -9,63 +9,55 @@ class BaseDeDatos::join_iterator{
   typedef list<Tabla::const_iterador_registros> listaReg;
   //Amigos de la clase
   friend class BaseDeDatos;
- 
-  //Constructor vacío
-  //Constructor por copia
-  //Operador =
-  //Operador ==
-  //Operador !=
-  //Operador ++ (prefijo)
-  //Operador ++int (postfijo)
-  //Operador *
-  //Operador ->
+
+  bool operator==(const join_iterator& otro) const{
+    return (_elem1 == otro._elem1 && _elem2 == otro._elem2 && _pos == otro._pos);
+  }
+  bool operator!=(const join_iterator& otro) const{
+    return not(*this == otro);
+  }
+
+
+
+  join_iterator& operator++(){
+  if (true){
+    listaReg::const_iterator* nuevoPos = &(*(_pos++));
+    const_iterador_registros* nuevoElem2 = &(*(*(nuevoPos)));
+
+    return join_iterator(_elem1,nuevoElem2,nuevoPos);
+  } else {
+    for (auto r = *(_elem1++); r != t1.registros_end(); ++r ) {
+      Dato d = r->dato(campo);
+      if (d.esNat()){
+        if(i._diccInt.count(d.valorNat())){
+          pos = i._diccInt[d.valorNat()].begin();
+          elem2 = *(pos);
+          const_iterador_registros* join1 = &r;
+          const_iterador_registros* join2 = &elem2;
+          listaReg::iterator* join3 = &pos;
+          return join_iterator(join1,join2,join3);
+        }
+      }else{
+        if(i._diccString.count(d.valorStr())){
+          pos = i._diccString[d.valorStr()].begin();
+          elem2 = *(pos);
+          const_iterador_registros* join1 = &r;
+          const_iterador_registros* join2 = &elem2;
+          listaReg::iterator* join3 = &pos;
+          return join_iterator(join1,join2,join3);
+        }
+			}
+		}
+    return join_end() //si salgo del for y no retorne el siguiente elemento, entonces estoy en end.
+      }
+  }
  
  private:
- join_iterator(const_iterador_registros it1, const_iterador_registros it2, listaReg::iterator pos): 
+ join_iterator(const const_iterador_registros* it1, const const_iterador_registros* it2, const listaReg::const_iterator* pos): 
   _elem1(it1),_elem2(it2),_pos(pos) {}
-  const_iterador_registros _elem1;
-  const_iterador_registros _elem2;
-  listaReg::iterator _pos;
+  const_iterador_registros* _elem1;
+  const_iterador_registros* _elem2;
+  listaReg::const_iterator* _pos;
 };
-/* #include "BaseDeDatos.h" */
-/* #include "const_iterador_registros.h" */
-/* #include <iostream> */
-
-/* using namespace std; */
-/* typedef Tabla::const_iterador_registros const_iterador_registros;  */
-
-/* class BaseDeDatos::join_iterator { */
-/*  public: */
-/*   friend class BaseDeDatos; */
-
-/*   join_iterator join(string& t1, string& t2, string campo){ */
-/*     Indice &i = _indices[t2][campo]; */
-/*     elem1& = t1.begin(); */
-/*     for (auto r : t1.registros()){ */
-/*       Dato &d = r.dato(campo); */
-/*       if (d.esNat()){ */
-/*         if(i.count(d.esNat())){ */
-/*           elem2 = i._diccInt[d.valorNat()][0]; */
-/*           break */
-/*         } */
-/*       } */
-/*     } */
-/*     posicion = 0; //creo un iterador que indica en que posicion de la lista estoy. */
-/*     return join_iterator(elem1,elem2,posicion) */
-/* } */
-/*   Registro &operator*(); */
-
-/*   join_iterator &operator++(); */
-
-/*  private: */
-/*   friend class BaseDeDatos; */
-
-/*   const_iterador_registros elem1; */
-/*   const_iterador_registros elem2; */
-/*   int posicion; */
-
-/* }; */
-
-
 
 #endif
